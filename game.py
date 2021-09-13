3d09047c05f3997500f1307700b60bfb79262a4f
SimoHellgren <hellgrensimo@gmail.com>
2021-09-13 18:52:35 +0300

    improved utils

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
-from typing import Set
-from itertools import groupby
+from typing import Set, Iterator, Tuple
+from itertools import groupby, chain
 from functools import partial
-from models import Player, Suspect, Weapon, Room, FactType, Fact, add_fact
+from models import Card, Player, Suspect, Weapon, Room, FactType, Fact, add_fact
 
 
 # utilities
@@ -15,9 +15,43 @@ def sure_lacks(facts: Set[Fact]):
 def maybes(facts: Set[Fact]):
     return filter(lambda x: (len(x.cards) > 1) and (x.facttype == FactType.HAS), facts)
 
+def explode(fact: Fact) -> Iterator[Tuple[Player, Card, FactType]]:
+    '''Explode the cards of a fact. Not that care must be taken in interpreting FactType.HAS'''
+    p, cs, ft = fact
+    for c in cs:
+        yield p,c,ft
+
 sorted_groupby = lambda func, it: groupby(sorted(it, key=func), func)
 
 group_by_player = partial(sorted_groupby, lambda x: x.player)
+
+
+def print_knowledge(facts):
+    owned = set(chain.from_iterable(map(explode, sure_has(facts))))
+    not_owned = set(chain.from_iterable(map(explode, sure_lacks(facts))))
+    maybe_owned = set(chain.from_iterable(map(explode, maybes(facts))))
+
+    print('Owned for sure:')
+    for player, data in sorted_groupby(lambda x: x[0], owned):
+        print('-', player.name)
+        print('\n'.join(' - ' + c.name for _,c,_ in data))
+
+    print('Not owned:')
+    for player, data in sorted_groupby(lambda x: x[0], not_owned):
+        print('-', player.name)
+        print('\n'.join(' - ' + c.name for _,c,_ in data))
+
+    # cards that someone owns aren't maybes
+    m1 = {c for c in maybe_owned if c[2] in {t[2] for t in owned}}
+
+    # cards that are not owned by someone, are not maybes
+    m2 = {c for c in m1 if c[:2] not in {t[:2] for t in not_owned}}
+
+    print('Perhaps owned:')
+    for player, data in sorted_groupby(lambda x: x[0], m2):
+        print('-', player.name)
+        print('\n'.join(' - ' + c.name for _,c,_ in data))
+
 
 def get_input(collection):
     for i, elem in enumerate(collection):
@@ -25,15 +59,6 @@ def get_input(collection):
 
     return input(f'Pick a {elem.__class__.__name__}: ')
 
-
-def print_grouped(facts: Set[Fact], grouper, msg=''):
-    for group, facts in grouper(facts):
-        print(group.name, msg)
-        for fact in facts:
-            for card in fact.cards:
-                print('-', card.name)
-
-    print()
 
 # game setup
 suspects = list(map(Suspect, (
@@ -54,7 +79,7 @@ rooms = list(map(Room, (
 )))
 
 
-all_cards = set((*suspects, *weapons, *rooms))
+all_cards = [*suspects, *weapons, *rooms]
 
 players = list(map(Player, ('You', 'Player 2', 'Player 3')))
 
@@ -74,12 +99,22 @@ def initialize():
 
     return facts
 
-def loop(facts):
-    while True:
-        print('_'*20)
 
-        print('Record a fact')
-        
+def see_card():
+    p = get_input(players)
+
+
+    for i, elem in enumerate(all_cards):
+        print(i, '-', elem.name)
+
+    c = input('Pick a Card (any card!): ')
+    
+    player = players[int(p)]
+    card = all_cards[int(c)]
+
+    return Fact(player, {card}, FactType.HAS)
+
+def observe():
         p = get_input(players)
 
         s = get_input(suspects)
@@ -96,15 +131,31 @@ def loop(facts):
         room = rooms[int(r)]
         facttype = FactType.HAS if int(ft) else  FactType.LACKS
 
-        facts = add_fact(Fact(player, {suspect, weapon, room}, facttype), facts)
+        return Fact(player, {suspect, weapon, room}, facttype)
 
+def play(facts):
+    while True:
+        print('_'*20)
+
+        print('Record a fact')
+        print('0 - observe')
+        print('1 - see a card')
+        c = input('0/1: ')
+
+        if c == '0':
+            func = observe
+
+        elif c == '1':
+            func = see_card
+        
+        facts = add_fact(func(), facts)
 
         print('You now know this:')
+        print_knowledge(facts)
 
-        print_grouped(sure_has(facts), group_by_player, 'has:')
-        print_grouped(sure_lacks(facts), group_by_player, 'does not have:')
-        print_grouped(maybes(facts), group_by_player, 'playsibly possesses:')
+        return facts
     
 if __name__ == '__main__':
     facts=initialize()
-    loop(facts)
+    while True:
+        facts = play(facts)
